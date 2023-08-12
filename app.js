e297f4a8c32ddcbbe6713f06489d7d232d6b120a
navtej bhatti <navbhtt0@gmail.com>
2023-08-13 00:36:10 +0530

    four package added passport, passport-local,  passport-local-mongoose, express-session

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
-const bcrypt = require("bcrypt");
+const session = require('express-session')
+const passport = require("passport")
+const passportLocalMongoose = require("passport-local-mongoose")
 
 
 const app = express();
@@ -15,7 +17,21 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
 
+
+app.use(session({
+    secret: "Be Vache Ni Vaat Modi PM Chhe.",
+    resave: false,
+    saveUninitialized: false,
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true,})
+
 
 
 const userSchema = new mongoose.Schema({
@@ -23,8 +39,16 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
+userSchema.plugin(passportLocalMongoose);
+
 
 const User = new mongoose.model("User", userSchema)
+
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 
@@ -38,55 +62,59 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
+app.get('/secrets', (req, res) => {
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("/login")
+    }
+     
+})
+
+app.get("/logout", (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            console.error("Error logging out:", err);
+            return res.redirect("/");
+        }
+        res.redirect("/");
+    });
+});
+
+
 app.post("/register", async function(req, res){
 
-  
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if(err){
+            console.log(err);
+            res.redirect("/register")
+        } else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets")
+            });
+        }
+    });
 
-  const newUser = new User({
-    email: req.body.username,
-     password: await bcrypt.hash(req.body.password, 10)
-  });
-
-  try {
-    await newUser.save();
-    res.render("secrets");
-  } catch (err) {
-    console.log(err);
-    // Handle the error appropriately, such as showing an error page
-  }
 });
 
 
 app.post("/login", async function(req, res) {
-    const username = req.body.username;
-    const plaintextPassword = req.body.password;
 
-    try {
-        const foundUser = await User.findOne({ email: username });
+    const user = new User({
+        username:req.body.username,
+        password:req.body.password
+    });
 
-        if (foundUser) {
-            // Compare the entered plaintext password with the hashed password from the database
-            bcrypt.compare(plaintextPassword, foundUser.password, function(err, result) {
-                if (err) {
-                    console.log(err);
-                    // Handle the error appropriately
-                    return;
-                }
-
-                if (result) {
-                    res.render("secrets"); // Successful login
-                } else {
-                    res.redirect("/login"); // Redirect to login page if authentication fails
-                }
-            });
-        } else {
-            // User not found
-            res.redirect("/login"); // Redirect to login page
-        }
-    } catch (err) {
+    req.login(user, function (err) {
+    if (err) {
         console.log(err);
-        // Handle the error appropriately
+    }else{
+        passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets")
+            });
     }
+    })
+    
 });
 
 
